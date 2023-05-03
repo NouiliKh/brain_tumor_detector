da60901857b0612683674ee955f2b61fb9b52fb2
NouiliKh <khalil.nouili@gmail.com>
2023-05-03 15:12:40 +0200

    update version

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/NouiliKh/brain_tumor_detector
 
-go 1.18
-
-require github.com/artdarek/go-unzip v1.0.0 // indirect
+go 1.20
--- pkg/utilities.go
+++ pkg/utilities.go
@@ -1,7 +1,79 @@
-package pkg
+package main
 
-import "fmt"
+import (
+	"archive/zip"
+	// "bufio"
+	// "fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
-func unzip_folder() {
-	fmt.Println("hello world")
+func UnZip(src, dest, ignoreDir string) error {
+
+	reader, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+
+	defer reader.Close()
+
+	for _, f := range reader.File {
+		rc, err := f.Open()
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+
+		defer rc.Close()
+
+		fpath := filepath.Join(dest, f.Name)
+
+		if lastIndex := strings.LastIndex(fpath, ignoreDir); lastIndex == -1 {
+			if f.FileInfo().IsDir() {
+				err = os.MkdirAll(fpath, 0755)
+				if err != nil {
+					log.Fatal(err)
+					return err
+				}
+			} else {
+				var fdir string
+				if lastIndex := strings.LastIndex(fpath, string(os.PathSeparator)); lastIndex > -1 {
+					fdir = fpath[:lastIndex]
+				}
+
+				if len(f.Name) < 150 {
+
+					err = os.MkdirAll(fdir, 0755)
+					if err != nil {
+						log.Fatal(err)
+						return err
+					}
+
+					fi, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+					if err != nil {
+						log.Fatal(err)
+						return err
+					}
+
+					defer fi.Close()
+
+					_, err = io.Copy(fi, rc)
+					if err != nil {
+						log.Fatal(err)
+						return err
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	UnZip("./data/mri_brain_tumor.zip", "./data/mri_brain_tumor", "")
+
 }
